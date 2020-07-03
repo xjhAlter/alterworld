cce4a7ec3e003c5d632c044da20c7791a78c5928
xuejiehe <1367270948@qq.com>
2020-07-03 11:03:28 +0800

    feat: 按钮样式&checkbox组件

--- src/components/common/checkbox/src/checkbox.vue
+++ src/components/common/checkbox/src/checkbox.vue
@@ -1,15 +1,185 @@
 <template>
-  <div>
-
+  <div @click="disabled && $emit('error')" class="aw-checkbox" :class="[{selected: cptModel, disabled: disabled}]">
+    <label>
+      <span class="aw-checkbox-icon"><div class="select-flag"></div></span>
+      <div class="aw-checkbox-name" :style="{'margin-left': gap + 'px'}">
+        <slot :statement="cptModel"></slot>
+      </div>
+      <input class="aw-checkbox-input" type="checkbox" v-model="cptModel" :disabled="disabled">
+    </label>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AwCheckbox'
+  name: 'AwCheckbox',
+  model: {
+    prop: 'checkboxModel',
+    event: 'change'
+  },
+  props: {
+    checkboxModel: {
+      required: true,
+      validator: (val) => {
+        return typeof val === 'boolean' || val === undefined || val instanceof Array
+      }
+    },
+    disabled: {
+      type: Boolean,
+      default: () => {
+        return false
+      }
+    },
+    path: {
+      type: Array,
+      validator: (val) => {
+        return val.length > 0
+      },
+      default: () => {
+        return ['checked']
+      }
+    },
+    gap: {
+      type: [String, Number],
+      default: '10'
+    }
+  },
+  methods: {
+  },
+  computed: {
+    cptModel: {
+      get () {
+        if (this.checkboxModel instanceof Array) {
+          let values = []
+          this.checkboxModel.forEach(obj => {
+            let value = obj
+            for (let i = 0; i < this.path.length; ++i) {
+              value = value[this.path[i]]
+            }
+            values.push(value)
+          })
+          return values.length && values.every(value => value)
+        }
+        return this.checkboxModel
+      },
+      set (val) {
+        if (this.checkboxModel instanceof Array) {
+          this.checkboxModel.forEach(obj => {
+            let value = obj
+            // 保持引用
+            for (let i = 0; i < this.path.length - 1; ++i) {
+              value = value[this.path[i]]
+            }
+            this.$set(value, [this.path[this.path.length - 1]], val)
+          })
+          this.$emit('change', this.checkboxModel)
+          return
+        }
+        this.$emit('change', val)
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style rel="stylesheet/scss" lang="scss" scoped>
+  *{
+    box-sizing: border-box;
+    cursor: pointer;
+    font-size: 14px;
+    white-space: nowrap;
+  }
+  .aw-checkbox{
+    display: inline-block;
+    line-height: 1;
+    &.aw-btn {
+      padding: 0;
+      label {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+      }
+      .aw-checkbox-icon {
+        display: none;
+      }
+      .aw-checkbox-name {
+        margin: 0 !important;
+      }
+    }
+    .aw-checkbox-icon {
+      background-color: #fff;
+      vertical-align: middle;
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      width: 14px;
+      height: 14px;
+      overflow: hidden;
+      border-radius: 2px;
+      border: #ccc 1px solid;
+      .select-flag {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 14px;
+        height: 14px;
+        background-color: #409eff;
+        color: #fff;
+        opacity: 0;
+        transform: scale(0.3);
+        &::before{
+          content: '';
+          border: 2px solid #fff;
+          border-top: 0;
+          border-left: 0;
+          width: 4px;
+          height: 8px;
+          transform: translateY(-1.5px) rotate(45deg) translateZ(0px); // translateZ防抖
+        }
+      }
+    }
+    .aw-checkbox-name {
+      display: inline-block;
+      vertical-align: middle;
+      &:empty {
+        margin: 0 !important;
+      }
+    }
+    .aw-checkbox-input {
+      display: none;
+    }
+    &:hover, &.selected {
+      .aw-checkbox-icon {
+        border-color: #409eff;
+      }
+    }
+    &.selected {
+      .aw-checkbox-icon {
+        .select-flag {
+          opacity: 1;
+          transform: scale(1);
+        }
+      }
+    }
+    &.disabled {
+      * {
+        cursor: no-drop;
+        color: #ccc;
+      }
+      &.selected {
+        .aw-checkbox-icon {
+          background-color: transparent;
+        }
+      }
+      .aw-checkbox-icon {
+        background-color: #eee;
+        border-color: #ccc;
+        .select-flag {
+          background-color: #ccc;
+        }
+      }
+    }
+  }
 </style>
--- src/modules/comp-doc/pages/checkbox.vue
+++ src/modules/comp-doc/pages/checkbox.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="doc-page">
     <doc-wrapper :doc-data="componentDoc">
+      <div class="example">
+        <div class="flex flex-column align-start ml-50">
+          <aw-checkbox class="aw-btn mr-10" v-model="objs" :path="['value']" v-slot="inner">
+            {{inner.statement?'取消全选':'全选'}}
+          </aw-checkbox>
+          <aw-checkbox v-for="(obj, index) in objs"
+                       class="mt-20"
+                       v-model="obj.value"
+                       :key="index"
+                       :disabled="obj.disabled"
+                       @change="onCheckboxChange"
+                       @error="handleChange">
+            {{obj.name}}
+          </aw-checkbox>
+        </div>
+        <div class="flex ml-100">
+          <span>当前选中：{{value}}</span>
+        </div>
+      </div>
     </doc-wrapper>
   </div>
 </template>
@@ -15,8 +34,103 @@ export default {
   data () {
     return {
       componentDoc: {
-        name: '复选框'
-      }
+        name: '复选框',
+        attributes: {
+          vModel: {
+            name: 'v-model',
+            description: '绑定值，用作全选时传入数组',
+            type: '',
+            optionalValues: '',
+            defaultValues: '',
+            required: true
+          },
+          path: {
+            name: 'path',
+            description: '全选模式时绑定值的访问路径',
+            type: 'String[]',
+            optionalValues: '',
+            defaultValues: '',
+            required: false
+          },
+          disabled: {
+            name: 'disabled',
+            description: '是否禁用该多选框',
+            type: 'Boolean',
+            optionalValues: '',
+            defaultValues: 'false',
+            required: false
+          },
+          gap: {
+            name: 'gap',
+            description: 'label与选框的间距',
+            type: 'String | Number',
+            optionalValues: '',
+            defaultValues: '10',
+            required: false
+          }
+        },
+        events: {
+          change: {
+            name: 'change',
+            description: '该多选框值变化后的回调',
+            returnValue: '该多选框的新值或传入的数组'
+          },
+          error: {
+            name: 'error',
+            description: 'disabled时点击触发',
+            returnValue: '-'
+          }
+        }
+      },
+      objs: [
+        {
+          name: '5月',
+          disabled: false,
+          value: false
+        },
+        {
+          name: '6月',
+          disabled: false,
+          value: true
+        },
+        {
+          name: '7月',
+          disabled: true,
+          value: false
+        },
+        {
+          name: '8月',
+          disabled: true,
+          value: true
+        }
+      ]
+    }
+  },
+  methods: {
+    /**
+     * checkbox值发生改变
+     * @param value
+     */
+    onCheckboxChange (value) {
+      console.log(value)
+    },
+    /**
+     * 点击禁用的checkbox
+     */
+    handleChange () {
+      console.log('这个checkbox被禁用了')
+    }
+  },
+  computed: {
+    value () {
+      const objs = this.objs
+      let v = []
+      objs.forEach(o => {
+        if (o.value) {
+          v.push(o.name)
+        }
+      })
+      return v.join(', ')
     }
   }
 }
@@ -28,7 +142,6 @@ export default {
     justify-content: space-between;
     .example{
       display: flex;
-      flex-direction: column;
       justify-content: flex-start;
       align-items: flex-start;
     }
