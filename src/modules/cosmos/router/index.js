292c722c7e3ecc1fb6405324d9968cb531cd293d
xuejiehe <1367270948@qq.com>
2020-08-05 10:05:53 +0800

    feat: 视差滚动

--- src/modules/cosmos/router/index.js
+++ src/modules/cosmos/router/index.js
@@ -1,10 +1,19 @@
 const fkw = () => import('../pages/fkw')
+const paraScroll = () => import('../pages/parallax-scrolling')
 
 export default [
   {
     name: 'fkw',
     path: '/cosmos/fkw',
     component: fkw,
+    meta: {
+      keepAlive: false
+    }
+  },
+  {
+    name: 'paraScroll',
+    path: '/cosmos/paraScroll',
+    component: paraScroll,
     meta: {
       keepAlive: false
     }
--- src/modules/home/pages/home.vue
+++ src/modules/home/pages/home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <h1 class="highlight">首页</h1>
     <button class="mt-30 aw-btn" @click="toCosmos('fkw')">cosmos fkw</button>
+    <button class="mt-30 ml-30 aw-btn" @click="toCosmos('paraScroll')">视差滚动</button>
   </div>
 </template>
 
