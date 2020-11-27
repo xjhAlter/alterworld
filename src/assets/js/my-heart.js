e87764cf6f3f0db75bdd6cdfd047ee32d10c1fc8
xuejiehe <1367270948@qq.com>
2020-11-27 15:04:33 +0800

    fix: 小心心样式调整

--- src/assets/js/my-heart.js
+++ src/assets/js/my-heart.js
@@ -2,8 +2,8 @@
 document.body.onmouseup = function (e) {
   if (e.button !== 0) return // 只有鼠标左键点击触发
   // 获取坐标
-  let x = e.pageX - 10
-  let y = e.pageY - 10
+  let x = e.pageX + 5
+  let y = e.pageY - 5
 
   // 获取随机颜色
   let color = 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')'
@@ -13,10 +13,10 @@ document.body.onmouseup = function (e) {
   let left = document.createElement('div')
   let right = document.createElement('div')
   let under = document.createElement('div')
-  heart.style = `width:20px;height:20px;opacity:1;position:absolute;z-index:4096;transition:2s;left:${x}px;top:${y}px;`
-  left.style = `width: 10px;height: 10px;border-radius: 100%;position: absolute;background:${color}`
-  right.style = `width: 10px;height: 10px;border-radius: 100%;position: absolute;right:0;background:${color}`
-  under.style = `width: 10px;height: 10px;position: absolute;top: 5px;left: 5px;transform: rotate(45deg);background:${color}`
+  heart.style = `width:15px;height:15px;opacity:1;position:absolute;z-index:4096;transition:2s;transform: rotate(45deg);left:${x}px;top:${y}px;`
+  left.style = `width: 10px;height: 10px;border-radius: 50%;position: absolute;left:0;top:5px;background:${color}`
+  right.style = `width: 10px;height: 10px;border-radius: 50%;position: absolute;left:5px;top:0;background:${color}`
+  under.style = `width: 10px;height: 10px;position: absolute;left:5px;top: 5px;background:${color}`
 
   // 将创建的元素添加到容器中
   heart.appendChild(left)
@@ -25,7 +25,7 @@ document.body.onmouseup = function (e) {
   document.body.appendChild(heart)
 
   setTimeout(() => {
-    heart.style.transform = 'translateY(-100px)'
+    heart.style.transform = 'translateY(-100px) rotate(45deg)'
     heart.style.opacity = '0'
     setTimeout(() => {
       document.body.removeChild(heart)
