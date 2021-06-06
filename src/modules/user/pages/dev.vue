bc2ca1fa712b41789c605408523709b661f95545
xjhAlter <1367270948@qq.com>
2021-06-06 16:22:39 +0800

    test

--- src/modules/user/pages/dev.vue
+++ src/modules/user/pages/dev.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="dev">
-    <div class="dev-row flex" style="background-color: #ebebeb;">
-      <progress-circle :values="progressData.values" :radius="100" :width="30" :bgColor="'#fff'"/>
+    <div class="dev-row flex">
+      <div class="div-box div-box-1 mr-10">20%</div>
+      <div class="div-box div-box-2 mr-10">20%</div>
     </div>
-    <div class="dev-row">
+    <div class="rem-box table-box">
+      <span class="table-cell-box">rem</span>
+    </div>
+    <div class="dev-row flex" style="background-color: #ebebeb" v-if="false">
+      <progress-circle
+        :values="progressData.values"
+        :radius="100"
+        :width="30"
+        :bgColor="'#fff'"
+      />
+    </div>
+    <div class="dev-row" v-if="false">
       <div class="clip-box"></div>
     </div>
     <div class="dev-row flex justify-around" v-if="false">
@@ -20,15 +32,15 @@
 </template>
 
 <script>
-import dropBox from '@components/dropBox'
-import progressCircle from '@components/progress-circle'
+import dropBox from "@components/dropBox";
+import progressCircle from "@components/progress-circle";
 export default {
-  name: 'dev',
+  name: "dev",
   components: {
     dropBox,
-    progressCircle
+    progressCircle,
   },
-  data () {
+  data() {
     return {
       box: {
         status: {
@@ -37,157 +49,336 @@ export default {
           left: 100,
           top: 20,
           opacity: 1,
-          rotate: 0
-        }
+          rotate: 0,
+        },
       },
       progressData: {
         values: [
-          { title: '第一阶段', value: 25, color: '#409eff' },
-          { title: '第二阶段', value: 25 },
-          { title: '第三阶段', value: 25 },
-          { title: '第四阶段', value: 25 }
-        ]
-      }
-    }
+          { title: "第一阶段", value: 25, color: "#409eff" },
+          { title: "第二阶段", value: 25 },
+          { title: "第三阶段", value: 25 },
+          { title: "第四阶段", value: 25 },
+        ],
+      },
+    };
   },
-  created () {
+  created() {
     // setTimeout(() => {
     //   this.$set(this.progressData.values[1], 'value', 100)
     // }, 3000)
+    this.test1();
   },
   methods: {
+    test1() {
+      // 注释里为输出的顺序
+      console.log(1);  // 1
+      let p = new Promise((resolve) => {
+        console.log(2)  // 2
+        resolve()
+        console.log(3)   // 3
+      })
+      setTimeout(() =>{
+        console.log(4)   // 5
+      })
+      p.then(()=>{
+        console.log(5)  // 4
+      });
+    },
 
   },
-  computed: {
-
-  }
-
-}
+  computed: {},
+};
 </script>
 
 <style lang="scss" scoped>
-  @keyframes clip {
-    0%,100%{
-      clip: rect(0px,220px,2px,0px);
-    }
-    25%{
-      clip: rect(0px,2px,220px,0px);
-    }
-    50%{
-      clip: rect(218px,220px,220px,0px);
-    }
-    75%{
-      clip: rect(0px,220px,220px,218px);
-    }
+@keyframes clip {
+  0%,
+  100% {
+    clip: rect(0px, 220px, 2px, 0px);
   }
-
-  @keyframes clipPath {
-    0%,100%{
-      /* 正三角形 */
-      background-color: #f00;
-      clip-path: polygon(50% 0%,100% 100%,100% 100%,100% 100%,100% 100%,100% 100%,100% 100%,0% 100%);
-    }
-    10%,90%{
-      /* 正四边形 */
-      background-color: #f60;
-      clip-path: polygon(0% 0%,100% 0%,100% 100%,100% 100%,100% 100%,100% 100%,100% 100%,0% 100%);
-    }
-    20%,80%{
-      /* 正五边形 */
-      background-color: #ff0;
-      clip-path: polygon(0% 38.31%,50% 0%,100% 38.31%,80.86% 100%,80.86% 100%,80.86% 100%,80.86% 100%,19.14% 100%);
-    }
-    30%,70%{
-      /* 正六边形 */
-      background-color: #0c0;
-      clip-path: polygon(0% 50%,25% 0%,75% 0%,100% 50%,75% 100%,75% 100%,75% 100%,25% 100%);
-    }
-    40%,60%{
-      /* 正七边形 */
-      background-color: #699;
-      clip-path: polygon(0% 64.22%,10.09% 19.72%,50% 0%,90.18% 19.72%,100% 64.22%,72.32% 100%,72.32% 100%,27.68% 100%);
-    }
-    50%{
-      /* 正八边形 */
-      background-color: #06c;
-      clip-path: polygon(0% 70.66%,0% 29.34%,29.34% 0%,70.66% 0%,100% 29.34%,100% 70.66%,70.66% 100%,29.34% 100%);
-    }
+  25% {
+    clip: rect(0px, 2px, 220px, 0px);
   }
+  50% {
+    clip: rect(218px, 220px, 220px, 0px);
+  }
+  75% {
+    clip: rect(0px, 220px, 220px, 218px);
+  }
+}
 
-  .dev{
-    position: relative;
+@keyframes clipPath {
+  0%,
+  100% {
+    /* 正三角形 */
+    background-color: #f00;
+    clip-path: polygon(
+      50% 0%,
+      100% 100%,
+      100% 100%,
+      100% 100%,
+      100% 100%,
+      100% 100%,
+      100% 100%,
+      0% 100%
+    );
+  }
+  10%,
+  90% {
+    /* 正四边形 */
+    background-color: #f60;
+    clip-path: polygon(
+      0% 0%,
+      100% 0%,
+      100% 100%,
+      100% 100%,
+      100% 100%,
+      100% 100%,
+      100% 100%,
+      0% 100%
+    );
+  }
+  20%,
+  80% {
+    /* 正五边形 */
+    background-color: #ff0;
+    clip-path: polygon(
+      0% 38.31%,
+      50% 0%,
+      100% 38.31%,
+      80.86% 100%,
+      80.86% 100%,
+      80.86% 100%,
+      80.86% 100%,
+      19.14% 100%
+    );
+  }
+  30%,
+  70% {
+    /* 正六边形 */
+    background-color: #0c0;
+    clip-path: polygon(
+      0% 50%,
+      25% 0%,
+      75% 0%,
+      100% 50%,
+      75% 100%,
+      75% 100%,
+      75% 100%,
+      25% 100%
+    );
+  }
+  40%,
+  60% {
+    /* 正七边形 */
+    background-color: #699;
+    clip-path: polygon(
+      0% 64.22%,
+      10.09% 19.72%,
+      50% 0%,
+      90.18% 19.72%,
+      100% 64.22%,
+      72.32% 100%,
+      72.32% 100%,
+      27.68% 100%
+    );
+  }
+  50% {
+    /* 正八边形 */
+    background-color: #06c;
+    clip-path: polygon(
+      0% 70.66%,
+      0% 29.34%,
+      29.34% 0%,
+      70.66% 0%,
+      100% 29.34%,
+      100% 70.66%,
+      70.66% 100%,
+      29.34% 100%
+    );
+  }
+}
+
+.dev {
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+  .dev-row {
     width: 100%;
-    overflow: hidden;
-    .dev-row{
-      width: 100%;
-      height: 300px;
-      position: relative;
-      padding: 20px;
-      border-bottom: 1px solid #d8d8d8;
-    }
+    height: 300px;
+    position: relative;
+    padding: 20px;
+    border-bottom: 1px solid #d8d8d8;
+  }
 
-    .clip-box{
-      width: 200px;
-      height: 200px;
-      margin: auto;
-      color: #69ca62;
-      box-shadow: inset 0 0 0 1px rgba(105,202,98,.5);
+  .clip-box {
+    width: 200px;
+    height: 200px;
+    margin: auto;
+    color: #69ca62;
+    box-shadow: inset 0 0 0 1px rgba(105, 202, 98, 0.5);
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    &::before,
+    &::after {
+      content: "";
+      z-index: 99;
+      margin: -5%;
+      box-shadow: inset 0 0 0 2px;
+      animation: clip 8s linear infinite;
       position: absolute;
       top: 0;
       left: 0;
       right: 0;
       bottom: 0;
-
-      &::before,
-      &::after{
-        content: '';
-        z-index: 99;
-        margin: -5%;
-        box-shadow: inset 0 0 0 2px;
-        animation: clip 8s linear infinite;
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-      }
-
-      &::before{
-        animation-delay: -4s;
-      }
-
-      &:hover::before,
-      &:hover::after{
-        background-color: #69ca62;
-      }
     }
 
-    .clip-path-box{
-      width: 200px;
-      height: 200px;
-      background-color: #409eff;
-
-      &.inset{
-        clip-path: inset(20px 40px 20px 40px round 10px);
-      }
-
-      &.circle{
-        clip-path: circle(50px at 100px 100px);
-      }
-
-      &.ellipse{
-        clip-path: ellipse(90px 60px at 50% 50%);
-      }
-
-      &.polygon{
-        clip-path: polygon(50% 0,80.86% 100%,0 38.31%,100% 38.31%,19.14% 100%);
-      }
+    &::before {
+      animation-delay: -4s;
     }
 
-    .anim-box{
-      width: 200px;
-      height: 200px;
-      animation: clipPath 8s linear infinite;
+    &:hover::before,
+    &:hover::after {
+      background-color: #69ca62;
     }
   }
+
+  .clip-path-box {
+    width: 200px;
+    height: 200px;
+    background-color: #409eff;
+
+    &.inset {
+      clip-path: inset(20px 40px 20px 40px round 10px);
+    }
+
+    &.circle {
+      clip-path: circle(50px at 100px 100px);
+    }
+
+    &.ellipse {
+      clip-path: ellipse(90px 60px at 50% 50%);
+    }
+
+    &.polygon {
+      clip-path: polygon(
+        50% 0,
+        80.86% 100%,
+        0 38.31%,
+        100% 38.31%,
+        19.14% 100%
+      );
+    }
+  }
+
+  .anim-box {
+    width: 200px;
+    height: 200px;
+    animation: clipPath 8s linear infinite;
+  }
+
+  .rem-box {
+    width: 10rem;
+    height: 5rem;
+    background-color: #409eff;
+  }
+
+  .table-box {
+    display: table-cell;
+    vertical-align: middle;
+  }
+
+  .div-box {
+    box-sizing: content-box;
+  }
+
+  @keyframes rotate1 {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  .div-box-1 {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 4px solid rgb(184, 187, 191);
+    border-radius: 100%;
+    width: 40px;
+    height: 40px;
+    font-size: 12px;
+    font-weight: bold;
+    color: rgb(172, 28, 177);
+    box-shadow: 0 1px 2px 1px rgba(0, 0, 0, 0.2),
+      inset 0 1px 2px 1px rgba(0, 0, 0, 0.2);
+    &::before {
+      content: "";
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      margin-left: -24px;
+      margin-top: -24px;
+      display: block;
+      border: 4px solid transparent;
+      border-top-color: rgb(172, 28, 191);
+      border-radius: 100%;
+      width: 40px;
+      height: 40px;
+      animation: rotate1 infinite 2s ease;
+    }
+  }
+
+  @keyframes rotate2 {
+    0% {
+      transform: rotate(0deg) translateY(-2px);
+    }
+    100% {
+      transform: rotate(360deg) translateY(-2px);
+    }
+  }
+
+  .div-box-2 {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100%;
+    width: 40px;
+    height: 40px;
+    background-image: linear-gradient(45deg, rgb(14, 14, 46), rgb(64, 64, 112));
+    font-size: 12px;
+    font-weight: bold;
+    color: white;
+    box-shadow: 0 3px 2px 1px rgba(0, 0, 0, 0.2);
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      margin-left: -20px;
+      margin-top: -20px;
+      width: 40px;
+      height: 40px;
+      background: rgb(51, 149, 230);
+      border-radius: 100%;
+      z-index: -1; // 被背景挡住了
+      transform: translateY(-2px);
+    }
+    &:before {
+      animation: rotate2 infinite 3s linear;
+    }
+    &:after {
+      animation: rotate2 infinite 2s linear;
+    }
+  }
+}
 </style>
--- src/modules/user/pages/user-info.vue
+++ src/modules/user/pages/user-info.vue
@@ -1,13 +1,120 @@
 <template>
-  <div></div>
+  <div class="test-mian">
+    <div class="test-box" style="width: 200px; height: 200px">
+      <div class="flex-box-1"></div>
+      <div class="flex-box-2"></div>
+    </div>
+    <div class="test-box">
+      <!-- 默认是content-box 但是项目改成border-box了 -->
+      <div class="box"></div>
+      <div class="box border-box">border-box</div>
+      <div class="box content-box">content-box</div> 
+    </div>
+    <div class="test-box">
+      <div class="parent">
+        <div class="child"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'user-info'
-}
+  name: "user-info",
+  data() {
+    return {
+      name: "Vue",
+    };
+  },
+  mounted() {
+    this.fun1();
+    // this.fun2();
+  },
+  methods: {
+    fun1() {
+      let obj = {
+        name: "Alter",
+        age: 25,
+        getName: () => {
+          let fn = () => {
+            console.log("fn-name", this.name); // 这里也是父级作用域的this，但父级是个箭头函数，继续往上，最后是vue实例
+          };
+          fn();
+
+          return this.name; // 这里的this指向vue实例的this, 找的是上层的this
+        },
+        getAge: function () {
+          let fn = () => {
+            console.log("fn-age", this.age);
+          };
+          fn();
+
+          return this.age;
+        },
+      };
+      let fooName = obj.getName;
+      console.log("obj.getName ", obj.getName()); // 指向vue实例中的this.name 否则是window
+      console.log("fooName ", fooName()); // 同上
+
+      let fooAge = obj.getAge;
+      console.log("obj.getAge ", obj.getAge()); // 25
+      // console.log("fooAge ", fooAge());  // 报错，this 是一个undefined
+    },
+    fun2() {
+      function foo1(value) {
+        let foo2 = (val) => {
+          console.log(arguments); // 没有自己的arguments，访问的是foo1的arguments
+        };
+        foo2(114);
+      }
+      foo1(10086);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.test-box {
+  display: flex;
+  position: relative;
+}
+.box {
+  width: 100px;
+  height: 100px;
+  padding: 20px;
+  margin: 20px;
+  border: 1px solid #409eff;
+}
+.border-box {
+  box-sizing: border-box;
+}
+.content-box {
+  box-sizing: content-box;
+}
+.flex-box-1 {
+  flex: 1 1 50px;
+  background-color: #409eff;
+  height: 100%;
+}
+.flex-box-2 {
+  flex: 1 0 auto;
+  background-color: #ff0000;
+  height: 100%;
+}
+.parent {
+  position: absolute;
+  width: 300px;
+  height: 300px;
+  margin: 20px;
+  border: 1px solid #409eff;
+}
 
+.child {
+  position: absolute;
+  top: 0;
+  left: 30px;
+  width: 100px;
+  height: 100px;
+  border: 1px solid #409eff;
+}
 </style>
